722263ff4c7db042f526e0deeff9e444e6e4c574
Osmann8 <148338475+Osmann8@users.noreply.github.com>
2024-12-10 14:04:33 +0300

    Update Algorithm Project.py

--- Algorithm Project.py
+++ Algorithm Project.py
@@ -1,11 +1,13 @@
 next_patient = True
-# Take information about protein and it's level
 
 total_patient = 0
+# hemophilia-a ve b'ye sahip olan hasta sayıları
 number_of_hemo_A, number_of_hemo_B = 0, 0
 number_of_severity_severe, number_of_severity_moderate, number_of_severity_mild  = 0, 0, 0
 number_of_inhibitors_present_hemo_A, number_of_inhibitors_present_hemo_B = 0, 0
+# Prophylaxis alan hemophilia-a ve hemophilia-b'lerin sayısı
 number_of_patients_receiving_prophylaxis_A, number_of_patients_receiving_prophylaxis_B = 0, 0
+# Moderate olarak prophylaxis'e girmiş olanların sayısı
 number_of_prophylaxis_with_severity_moderate = 0
 
 while next_patient:
@@ -14,7 +16,7 @@ while next_patient:
     name = input("Enter patient's name: ")
     surname = input("Enter patient's surname: ")
 
-    # Information about disease
+    # Deficient protein type
     deficient_protein = int(input("Enter the deficient factor protein's number (8 or 9): "))
     while deficient_protein not in [8, 9]:
         deficient_protein = int(input("Enter the deficient factor protein's number (8 or 9): "))
@@ -23,6 +25,7 @@ while next_patient:
     deficient_factor_protein_level = float(input(f"Factor-{deficient_protein} level in blood (0-49): "))
     while deficient_factor_protein_level >= 50 or deficient_factor_protein_level < 0:
         deficient_factor_protein_level = float(input(f"Factor-{deficient_protein} level in blood (0-49): "))
+
 
     number_of_antibody = float(input("The amount of antibody in blood produced against factor medication (BU): "))
     if number_of_antibody < 0:
@@ -36,13 +39,13 @@ while next_patient:
     #Severity
     if deficient_factor_protein_level < 1:
         severity = "severe"
-        severity_severe += 1
+        number_of_severity_severe += 1
     elif deficient_factor_protein_level>=1 and deficient_factor_protein_level<=5:
         severity = "moderate"
-        severity_moderate += 1
+        number_of_severity_moderate += 1
     else:
         severity = "mild"
-        severity_mild += 1
+        number_of_severity_mild += 1
 
     if severity == "moderate":
         #number of bleeding in the past year
@@ -60,7 +63,7 @@ while next_patient:
             factor_production_type = input("Enter (P/p/R/r) for the production type of factor"
                                            " medication to be used (Plasma-derived/Recombinant): ")
         if severity == "moderate":
-            prophylaxis_with_severity_moderate += 1
+            number_of_prophylaxis_with_severity_moderate += 1
         prophylaxis = True
 
     print(f"ID: {ID} \nName:{name} \nSurname:{surname}")
@@ -99,50 +102,102 @@ while next_patient:
         print(f"Minimum required dose of medication at one time is: {minimum_dose_medication}")
 
         # Amount of medication to be used at one time (IU), types and quantities of vials
-        vial_sizes = [2000, 1500, 1000, 500, 250]
-        vials_used = []
-        for vial in vial_sizes:
-            count = minimum_dose_medication / vial
-            if int(count) > 0:
-                if vial == 250:
-                    if count != int(count):
-                        minimum_dose_medication -= (int(count) + 1) * vial
-                        vials_used.append((vial, int(count) + 1))
-                    else:
-                        vials_used.append((vial, int(count)))
-                        minimum_dose_medication -= int(count) * vial
-                else:
-                    vials_used.append((vial, int(count)))
-                    minimum_dose_medication -= int(count) * vial
-        total_amount_medication = sum([vial * count for vial, count in vials_used])
-        print(f"Amount of medication to be used at one time:{total_amount_medication}\n"
-              f"Used vial types and quantities at one time: ")
-        for vial, count in vials_used:
-            print(f"    {count} vial of {vial}")
+        a = minimum_dose_medication / 250
+        amount_of_total_medication = 0
+        dose_2000, dose_1500, dose_1000, dose_500, dose_250 = 0, 0, 0, 0, 0
+        while a > 0:
+            remaining_required_dose = minimum_dose_medication
+            a = minimum_dose_medication / 250
+            if a > 7:
+                dose_2000 += 1
+                minimum_dose_medication -= 2000
+                amount_of_total_medication += 2000
+            elif a > 5 and int(a) <= 7:
+                dose_1500 += 1
+                minimum_dose_medication -= 1500
+                amount_of_total_medication += 1500
+            elif a > 3 and int(a) <= 5:
+                dose_1000 += 1
+                minimum_dose_medication -= 1000
+                amount_of_total_medication += 1000
+            elif a > 1 and int(a) <= 3:
+                dose_500 += 1
+                minimum_dose_medication -= 500
+                amount_of_total_medication += 500
+            elif a > 0 and int(a) <= 1:
+                dose_250 += 1
+                minimum_dose_medication -= 250
+                amount_of_total_medication += 250
+
+        print(f"Amount of medication to be used: {amount_of_total_medication}. Type and quantities of vials: ", end="")
+        if dose_250 > 0:
+            print(f"{dose_250} vial of 250 IU", end="")
+            if dose_1000 or dose_500 or dose_2000 or dose_1500:
+                print(",", end="")
+            else:
+                print()
+        if dose_500 > 0:
+            print(f"{dose_500} vial of 500 IU", end="")
+            if dose_1000 or dose_2000 or dose_1500:
+                print(",", end="")
+            else:
+                print()
+        if dose_1000 > 0:
+            print(f"{dose_1000} vial of 1000 IU", end="")
+            if dose_2000 or dose_1500:
+                print(",", end="")
+            else:
+                print()
+        if dose_1500 > 0:
+            print(f"{dose_1500} vial of 1500 IU", end="")
+            if dose_2000:
+                print(",", end="")
+            else:
+                print()
+        if dose_2000 > 0:
+            print(f"{dose_2000} vial of 2000 IU")
 
         # Total amount of medication for 4 weeks, types and quantities of vials
         total_medication_for_month = 0
         if deficient_protein == 8:
-            total_medication_for_month += total_amount_medication * 12
-            print(f"Total amount for 4 weeks: {total_medication_for_month}\n"
-                  f"Used vials and quantities:")
-            for vial, count in vials_used:
-                print(f"    {count*12} vial of {vial}")
+            total_medication_for_month += amount_of_total_medication * 12
+            print(f"Total amount for 4 weeks: {total_medication_for_month} IU\n"
+                  f"Used vials and quantities: ",end="" )
+            # Bu işte bi yanlışlık var gibi ama :
+            if dose_250 > 0:
+                print(f"{dose_250} vial of 250 IU")
+            if dose_500 > 0:
+                print(f"{dose_500} vial of 500 IU")
+            if dose_1000 > 0:
+                print(f"{dose_1000} vial of 1000 IU")
+            if dose_1500 > 0:
+                print(f"{dose_1500} vial of 1500 IU")
+            if dose_2000 > 0:
+                print(f"{dose_2000} vial of 2000 IU")
+
 
         elif deficient_protein == 9:
-            total_medication_for_month += total_amount_medication * 8
-            print(f"Total amount for 4 weeks: {total_medication_for_month}\n"
+            total_medication_for_month += amount_of_total_medication * 8
+            print(f"Total amount for 4 weeks: {total_medication_for_month} IU\n"
                   f"Used vials and quantities:")
-            for vial, count in vials_used:
-                print(f"    {count * 8} vial of {vial}")
+            if dose_250 > 0:
+                print(f"{dose_250} vial of 250 IU")
+            if dose_500 > 0:
+                print(f"{dose_500} vial of 500 IU")
+            if dose_1000 > 0:
+                print(f"{dose_1000} vial of 1000 IU")
+            if dose_1500 > 0:
+                print(f"{dose_1500} vial of 1500 IU")
+            if dose_2000 > 0:
+                print(f"{dose_2000} vial of 2000 IU")
 
         # Total medication cost for 4 weeks
-        if factor_production_type in ["P","p"]:
+        if factor_production_type == "Plasma-derived":
             total_medication_cost = total_medication_for_month * 0.3
-            print(f"Total medication cost for 4 weeks: {total_medication_cost}")
-        elif factor_production_type in ["R","r"]:
+            print(f"Total medication cost for 4 weeks: {total_medication_cost}$")
+        elif factor_production_type == "Recombinant":
             total_medication_cost = total_medication_for_month * 0.4
-            print(f"Total medication cost for 4 weeks: {total_medication_cost}")
+            print(f"Total medication cost for 4 weeks: {total_medication_cost}$")
     else:
         print("Prophylaxis won't be provided")
 
@@ -179,7 +234,7 @@ print(f"Percentage of inhibitor presence in Hemophilia-B: {inhib_presence_B_perc
 # find out if it's asking for total percentage or hemophilia's inself
 percentage_of_patients_receiving_prophylaxis_A = number_of_patients_receiving_prophylaxis_A * 100 / number_of_hemo_A
 percentage_of_patients_receiving_prophylaxis_B = number_of_patients_receiving_prophylaxis_B * 100 / number_of_hemo_B
-print(f"Number of Hemophlia-A patients receiving prophylaxis: {number_of_patients_receiving_prophylaxis_A}, Percentage: {}% ")
+print(f"Number of Hemophlia-A patients receiving prophylaxis: {number_of_patients_receiving_prophylaxis_A}, Percentage: {percentage_of_patients_receiving_prophylaxis_A}% ")
 
 #  The percentage of patients receiving prophylaxis among hemophilia patients whose disease severity is moderate
 percentage_of_moderate_patients_in_prophylaxis = number_of_prophylaxis_with_severity_moderate * 100 / (number_of_patients_receiving_prophylaxis_A + number_of_patients_receiving_prophylaxis_B)
